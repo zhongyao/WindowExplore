5baff90f728e9a27fe7df0430114374d7817a9bf
zhongyao <zhongyao@alibaba-inc.com>
2017-12-15 14:51:24 +0800

    Initial commit

---
+++ app/src/main/java/com/hongri/window/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hongri.window;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author hongri
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
